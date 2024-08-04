c96c96bcd9e8e51e9cfc2ab3594c9b1b6a378e04
M Fahad Ahmed <35274399-mfahadahmed2012@users.noreply.replit.com>
2024-08-04 15:58:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const display =document.getElementById("display");
+
+function calculate(input) {
+     display.value += input;
+}
+
+
+
+
+function clearDisplay() {
+    display.value = "";
+
+}
+
+function calculator() {
+     try{
+     display.value = eval(display.value);
+ }
+     catch(error){
+     display.value = "Error"
+    }
+
+ }
+
+
+
+const display = document.getElementById("display");
+
+function calculate(input) {
+    display.value += input; 
+
+}
+function calculator(){
+    try{
+        display.value =eval(display.value)
+    }
+    catch{
+        display.value ="Error"
+    }
+}
+
+function clearDisplay() {
+    display.value = "";
+
+}
